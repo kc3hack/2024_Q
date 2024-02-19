bef023c84b995899aca3a39f21c5add99cc933b9
yudai-kdix <yudai.homma222@gmail.com>
2024-02-19 21:51:07 +0900

    投稿のデータベースを管理するクラスの追加
    画像の追加はあとで考えます…

---
+++ post.py
@@ -0,0 +1,39 @@
+from db.database import Table
+
+class Posts():
+    table_name = "post"
+    def __init__(self) -> None:
+        self.table = Table()
+        self.column_list = {
+            "title":"TEXT",
+            "content":"TEXT",
+            "user_id":"INTEGER",
+            "state":"INTEGER"
+        }
+        self.conn = self.table.create_connection()
+        self.table.create_table(self.conn,Post.table_name,self.column_list)
+
+    def create_post(self,title,content,user_id):
+        item_list = {
+            'title':title,
+            'content':content,
+            'user_id':user_id,
+            'state':0
+        }
+        item_lists = []
+        item_lists.append(item_list)
+        self.table.insert_table(self.conn,item_lists)
+    
+    def delete_post(self,id):
+        self.table.destroy_record_id(self.conn,id)
+    
+    def delete_user_post(self,user_id):
+        self.table.destroy_record(self.conn,f"user_id={user_id}")
+    
+    def get_post(self,id):
+        rows = self.table.select_table(self.conn,f"id={id}")
+        return rows
+    
+    def get_posts(self,condition):
+        rows = self.table.select_table(self.conn,condition)
+        return rows
