6c95214388e65c1614b4d3cf96ea945eeca5b25c
Inlet-back <mikeneko41@icloud.com>
2024-02-21 16:45:43 +0900

    テスト途中(インサートから)

--- db_test.py
+++ db_test.py
@@ -1,42 +1,43 @@
 from db.database import Table
 table = Table()
 class Test():
-   
+    table_name = 'test'
     def __init__(self) -> None:
         self.table = Table()
+        
         self.column_list = {
             "table1":"TEXT",
             "table2":"TEXT",
-            "id":"INTEGER",
+            "test_id":"INTEGER",
             "state":"INTEGER"
         }
+        print('new')
     
         self.conn = table.create_connection('test_data.db')
-        self.table.create_table(self.conn,'test',self.column_list)
-    
+        print('connect')
+        self.table.create_table(self.conn,Test.table_name,self.column_list)
+        print('create')
+
     def check_test(self,tablename):
         print(self.table.check_table(self.conn,tablename))
 
-    # def insert_test(self,table1,table2,id,state):
-    #     item_list ={
-    #         'table1':table1,
-    #         'table2':table2,
-    #         'id':id,
-    #         'state':state
-    #     }
-    #     item_lists = []
-    #     item_lists.append(item_list)
-    #     self.table.insert_table(self.conn,item_lists)
-    #     print('insert')
+    def insert_test(self,table1,table2,id,state):
+        item_list ={
+            'table1':table1,
+            'table2':table2,
+            'test_id':id,
+            'state':state
+        }
+        item_lists = []
+        item_lists.append(item_list)
+        self.table.insert_table(self.conn,item_lists)
+        print('insert')
     
     # def select_all_test(self):
     #     print(self.table.select_all_table(self.conn))
 
 test = Test()
 test.check_test('test')
-# test.create_table_test(conn)
-
-# test.check_test('test')
-# test.check_test('fake')
-# test.insert_test('test11','test12',1,1)
+test.check_test('fake')
+test.insert_test('test11','test12',1,1)
 # test.select_all_test()
